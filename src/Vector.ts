a6a8f34e9a4357dd25f7a355d7675dbb3810ae50
Daniel-I-Am <djdecloet@gmail.com>
2018-11-22 17:25:35 +0100

    vectors done

--- src/Vector.ts
+++ src/Vector.ts
@@ -5,11 +5,41 @@ class Vector {
         this.numbers = args;
     }
 
-    public getValue() {
+    public getValue(): Array<number> {
         return this.numbers;
     }
 
-    public getSize() {
-        return this.numbers.map(e => e**2)
+    public getSize(): number {
+        return Math.sqrt(this.numbers.map(e => e**2).reduce((a,b) => a+b, 0));
+    }
+
+    public getDim(): number {
+        return this.numbers.length;
+    }
+
+    public add(vector: Vector): Vector {
+        if (this.getDim() != vector.getDim())
+            throw new VectorDimError("Dimension of vector does not match.", `${this.getDim()} != ${vector.getDim()}`);
+        let myValue = this.getValue(),
+            thatValue = vector.getValue();
+        return new Vector(...myValue.map((e,i) => e+thatValue[i]));
+    }
+
+    public multiply(scalar: number): Vector {
+        return new Vector(...this.getValue().map(e => e*scalar));
+    }
+
+    public toString(): string {
+        return `[${this.getValue().map(e => e.toString()).join(", ")}]`
+    }
+}
+
+class VectorDimError {
+    private message: string;
+    private name: string;
+
+    public constructor(...args: Array<string>) {
+        this.name = "VectorDimError";
+        this.message = args.join("\n")
     }
 }
