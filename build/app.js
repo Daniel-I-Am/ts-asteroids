b7eb529f931f98efafd1140e57e628dca23f3b24
Daniel-I-Am <djdecloet@gmail.com>
2018-11-22 17:39:38 +0100

    looping

--- build/app.js
+++ build/app.js
@@ -12,6 +12,14 @@ class Entity {
         let velocity = this.velocity.getValue();
         this.location.x += velocity[0];
         this.location.y += velocity[1];
+        if (this.location.x < 0)
+            this.location.x = this.canvasHelper.getWidth();
+        if (this.location.x > this.canvasHelper.getWidth())
+            this.location.x = 0;
+        if (this.location.y < 0)
+            this.location.y = this.canvasHelper.getHeight();
+        if (this.location.y > this.canvasHelper.getHeight())
+            this.location.y = 0;
     }
 }
 class Asteroid extends Entity {
